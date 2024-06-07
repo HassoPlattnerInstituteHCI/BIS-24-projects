f495c0a1fc7273e6d45db3fd5d6ba1c446586d51
Mehdi Gouasmi <mgouasmi@web.de>
2024-06-07 02:06:18 +0200

    Add greedy AI

---
+++ Assets/Scripts/GreedyAI.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * 
+ */
+
+public class GreedyAI : MonoBehaviour
+{
+    public float speed = 2f;
+
+    private Rigidbody rb;
+    private Vector3 direction;
+    private GameObject ball;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        direction = Vector3.forward;
+
+        ball = GameObject.Find("Ball");
+    }
+
+    void FixedUpdate() {
+        Vector3 ballDirection = ball.GetComponent<Ball>().GetDirection();
+        if (IsBallHeadingTowardsMe()) {
+            MoveTowardsBall();
+        }
+        else {
+            MoveTowardsCenter();
+        }
+        rb.velocity = direction.normalized * 100 * (speed * Time.fixedDeltaTime);
+    }
+
+    bool IsBallHeadingTowardsMe() {
+        Vector3 ballDirection = ball.GetComponent<Ball>().GetDirection();
+        return ballDirection.x > 0;
+    }
+
+    void MoveTowardsBall() {
+        direction = ball.transform.position.z > this.transform.position.z 
+            ? Vector3.forward
+            : Vector3.back;
+    }
+
+    void MoveTowardsCenter() {
+        direction = (-0.2f < this.transform.position.z && this.transform.position.z < 0.2f)
+            ? Vector3.back
+            : Vector3.forward;
+    }
+}
