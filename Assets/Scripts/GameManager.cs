254fc3be562262d811243dec9523111a9b0d9d6e
SeadragonK1 <tom.kessler@ggl.schule>
2024-06-27 17:03:16 +0200

    moved to starting pos

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,21 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SpeechIO;
+using UnityEngine;
 
+namespace DualPantoToolkit{
 public class GameManager : MonoBehaviour
 {
     public int mode=0; 
     public SpeechOut speechIO= new SpeechOut();
     public GameObject PL;
+
+    LowerHandle l;
+    UpperHandle u;
     public GameObject PU;
     void Start()
     {
+        l = GameObject.Find("Panto").GetComponent<LowerHandle>();
+        u = GameObject.Find("Panto").GetComponent<UpperHandle>();
         StartCoroutine(DelayedStart());
     }
     IEnumerator DelayedStart(){
         yield return new WaitForSeconds(4);
         PL=GameObject.Find("ItHandle");
         PU=GameObject.Find("MeHandle");
+        l.MoveToPosition(GameObject.Find("Goal").transform.position - PL.transform.position, 2, false);
+        u.MoveToPosition(GameObject.Find("Ball").transform.position - PL.transform.position, 2, false);
         speechIO.Speak("Shoot the ball");
         this.GetComponent<Wiggle>().wiggle_wiggle_wiggle(true);
         yield return new WaitForSeconds(4);
@@ -29,4 +38,6 @@ public class GameManager : MonoBehaviour
         this.GetComponent<Wiggle>().wiggle_wiggle_wiggle(false);
         yield return new WaitForSeconds(4);
     }
+}
+
 }
