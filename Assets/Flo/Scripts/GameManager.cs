947f8635905818cb99b6bf924969931fc2e87172
tiramiezu <florian.munkelt@gmail.com>
2024-06-27 18:16:53 +0200

    create branch

---
+++ Assets/Flo/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using DualPantoToolkit;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public GameObject player;
+    public GameObject enemy;
+
+    public Transform projectileSpawn;
+    public Transform playerSpawn;
+    public Transform enemySpawn;
+    
+    private UpperHandle _upperHandle;
+    private LowerHandle _lowerHandle;
+    
+    PantoCollider[] pantoColliders;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _upperHandle = GetComponent<UpperHandle>();
+        _lowerHandle = GetComponent<LowerHandle>();
+        
+        // TODO 1: remove this comment-out
+        Introduction();
+    }
+    
+    async void Introduction()
+    {
+        Level level = GetComponent<Level>();
+        await level.PlayIntroduction(0.2f, 3000);
+        await Task.Delay(1000);
+        
+        // TODO 2:
+        await StartGame();
+    }
+
+    async Task StartGame()
+    {
+        await Task.Delay(1000);
+
+        // TODO 4: activate PlayerWall game object at Unity editor, and remove this comment-out
+        await RenderObstacle();
+        
+        await Task.Delay(1000);
+        
+        Instantiate(player, playerSpawn);
+        Instantiate(enemy, new Vector3(0.35f, 0.0f, -5.64f), Quaternion.identity);
+        GameObject sb = Instantiate(projectile, projectileSpawn);
+        
+        // TODO 3:
+        await _lowerHandle.SwitchTo(sb, 50.0f);
+        _upperHandle.Free();
+    }
+
+    async Task RenderObstacle()
+    {
+        pantoColliders = GameObject.FindObjectsOfType<PantoCollider>();
+        foreach (PantoCollider collider in pantoColliders)
+        {
+            collider.CreateObstacle();
+            collider.Enable();
+        }
+    }
+}
---
+++ Assets/Flo/Scripts/projectileSpawn.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        /* move to player
+        allign rotation with player */
+    }
+}
