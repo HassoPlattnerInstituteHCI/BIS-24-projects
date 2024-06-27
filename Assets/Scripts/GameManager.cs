1d739027e2d4d3eb8ac66170c20c20910eed695f
SeadragonK1 <tom.kessler@ggl.schule>
2024-06-27 18:48:50 +0200

    detect handle rotation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using SpeechIO;
@@ -12,6 +13,7 @@ public class GameManager : MonoBehaviour
     public GameObject PL;
     public GameObject PU;
     public GameObject forcefield;
+    float rotation_handle;
     Wiggle wiggle;
     LowerHandle l;
     UpperHandle u;
@@ -19,6 +21,7 @@ public class GameManager : MonoBehaviour
     GameObject RightBound;
     GameObject TopBound;
     GameObject BottomBound;
+    bool shooting = false;
     void Start()
     {
         wiggle=this.GetComponent<Wiggle>();
@@ -28,16 +31,29 @@ public class GameManager : MonoBehaviour
         RightBound=GameObject.Find("RightBound");
         TopBound=GameObject.Find("TopBound");
         BottomBound=GameObject.Find("BottomBound");
+        PL=GameObject.Find("ItHandle");
+        PU=GameObject.Find("MeHandle");
+        rotation_handle = 0f;
         StartCoroutine(DelayedStart());
     }
+    void Update()
+    {
+        if(Math.Abs(rotation_handle - l.GetRotation()) > 50)
+        {
+            if(Math.Abs(rotation_handle - l.GetRotation()) < 300)
+            {
+                shooting = true;
+            }
+        }
+        rotation_handle = l.GetRotation();
+    }
+
     IEnumerator DelayedStart(){
         LeftBound.SetActive(false);
         RightBound.SetActive(false);
         TopBound.SetActive(false);
         BottomBound.SetActive(false);
         yield return new WaitForSeconds(3);
-        PL=GameObject.Find("ItHandle");
-        PU=GameObject.Find("MeHandle");
         l.MoveToPosition(GameObject.Find("Goal").transform.position - PL.transform.position, 2, false);
         u.MoveToPosition(GameObject.Find("Ball").transform.position - PL.transform.position, 2, false);
         speechIO.Speak("Shoot the ball");
@@ -54,5 +70,4 @@ public class GameManager : MonoBehaviour
         Instantiate(forcefield,new Vector3(0,0,-7.3f),Quaternion.identity);
     }
 }
-
 }
