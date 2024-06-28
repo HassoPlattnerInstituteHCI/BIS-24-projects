6fcd98ffecef39d3cfad91a9f6a2b03d9a9e329f
corvin-koegler <corvin.koegler+github@gmail.com>
2024-06-28 22:23:32 +0200

    limited movementdetection to bounds

--- Assets/Scripts/MovementDetector.cs
+++ Assets/Scripts/MovementDetector.cs
@@ -6,18 +6,30 @@ public class MovementDetector : MonoBehaviour
 {
     private Vector3 lastPosition;
     public bool IsMoving { get; private set; }
+    public GameObject boundsArea;
+    private Collider boundsCollider;
+
 
     // Start is called before the first frame update
     void Start()
     {
         lastPosition = transform.position;
         IsMoving = false;
+
+        if (boundsArea != null)
+        {
+            boundsCollider = boundsArea.GetComponent<Collider>();
+            if (boundsCollider == null)
+            {
+                Debug.LogError("No Collider component found on boundsArea GameObject.");
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position != lastPosition)
+        if (transform.position != lastPosition && IsWithinBounds(transform.position))
         {
             IsMoving = true;
             lastPosition = transform.position;
@@ -26,5 +38,14 @@ public class MovementDetector : MonoBehaviour
         {
             IsMoving = false;
         }
+    }
+
+    bool IsWithinBounds(Vector3 position)
+    {
+        if (boundsCollider != null)
+        {
+            return boundsCollider.bounds.Contains(position);
+        }
+        return false;
     }
 }
