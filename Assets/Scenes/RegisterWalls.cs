4b33ad6503922286b37a8e5ecf1b10f7c34935a9
marekmp3 <marek.anton.stein@gmail.com>
2024-07-14 13:13:06 +0200

    sounds added

---
+++ Assets/Scenes/RegisterWalls.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using DualPantoToolkit;
+
+public class RegisterWalls : MonoBehaviour
+{
+    PantoHandle meHandle;
+
+    async void Start()
+    {
+        meHandle = GameObject.Find("Panto").GetComponent<UpperHandle>();
+        await meHandle.MoveToPosition(new Vector3(2, 0, -5), 20f);
+        await Task.Delay(5000);
+        PantoCollider[] pantoColliders = GameObject.FindObjectsOfType<PantoCollider>();
+        foreach (PantoCollider collider in pantoColliders)
+        {
+            collider.CreateObstacle();
+            collider.Enable();
+        }
+    }
+}
