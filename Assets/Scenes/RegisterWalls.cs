87b6fbe3674389b57d74f4f87e6b5b22e2ccc08e
marekmp3 <marek.anton.stein@gmail.com>
2024-07-14 14:53:52 +0200

    Level Introduction

--- Assets/Scenes/RegisterWalls.cs
+++ Assets/Scenes/RegisterWalls.cs
@@ -1,21 +1,33 @@
 ﻿using System.Threading.Tasks;
 using UnityEngine;
 using DualPantoToolkit;
+using SpeechIO;
 
 public class RegisterWalls : MonoBehaviour
 {
     PantoHandle meHandle;
+    private SpeechOut speechOut;
+
+    private void Awake()
+    {
+        speechOut = new SpeechOut();
+    }
 
     async void Start()
     {
+        await speechOut.Speak("This is the ball.");
         meHandle = GameObject.Find("Panto").GetComponent<UpperHandle>();
-        await meHandle.MoveToPosition(new Vector3(2, 0, -5), 20f);
-        await Task.Delay(5000);
+
+        await Task.Delay(3000);
+        await speechOut.Speak("This is you.");
+        await meHandle.MoveToPosition(new Vector3(2, 0, -6), 4f);
+        await speechOut.Speak("Find the whole and push the ball into the whole. Do it");
         PantoCollider[] pantoColliders = GameObject.FindObjectsOfType<PantoCollider>();
         foreach (PantoCollider collider in pantoColliders)
         {
             collider.CreateObstacle();
             collider.Enable();
         }
+
     }
 }
