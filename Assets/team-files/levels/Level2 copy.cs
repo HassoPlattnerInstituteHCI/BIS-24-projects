d336c3e397b75f4394b2f418b09b35d52fbe43a3
Henriette Blumreiter <henriette.blumreiter@student.hpi.uni-potsdam.de>
2025-01-07 14:38:59 +0100

    level 2 copy auskommentiert

--- Assets/team-files/levels/Level2 copy.cs
+++ Assets/team-files/levels/Level2 copy.cs
@@ -1,81 +1,79 @@
 
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using SpeechIO;
 
+// public class Level2 : MonoBehaviour
+// {
+//     private GameObject[] objects;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using SpeechIO;
+//     private bool finished = false;
 
-public class Level2 : MonoBehaviour
-{
-    private GameObject[] objects;
+//     public GameObject levelManagerObject;
 
-    private bool finished = false;
+//     private ObjectSelector oS;
 
-    public GameObject levelManagerObject;
+//     private SpeechOut speechOut;
 
-    private ObjectSelector oS;
+//     private string name1 = "";
 
-    private SpeechOut speechOut;
+//     private string name2 = "";
 
-    private string name1 = "";
+//     void Start()
+//     {
+//         speechOut = new SpeechOut();
 
-    private string name2 = "";
+//         oS = GameObject.FindObjectsOfType<ObjectSelector>()[0];
+//         oS.objectsSelectable = true;
+//         oS.objectsPlaceable = true;
+//         oS.wallPlaceable = false;
+//         oS.removeToolActivated = false;
+//         oS.doorToolActivated = false;
 
-    void Start()
-    {
-        speechOut = new SpeechOut();
+//         // speechOut.Speak("Level 2... Turn the lower handle to select objects... Place three different objects by turning the upper handle.");
+//         oS.soundLocked = true;
+//         speechOut.Speak("Find the key to open the door. ");
+//         Invoke("unlockSound", 10);
+//         Invoke("saySecondPart", 20);
+//     }
 
-        oS = GameObject.FindObjectsOfType<ObjectSelector>()[0];
-        oS.objectsSelectable = true;
-        oS.objectsPlaceable = true;
-        oS.wallPlaceable = false;
-        oS.removeToolActivated = false;
-        oS.doorToolActivated = false;
+//     public void objectPlaced(string name)
+//     {
+//         if (name1 == "")
+//         {
+//             name1 = name;
+//         }
+//         else if (name2 == "")
+//         {
+//             name2 = name;
+//         }
+//         else if (!finished && name1 != name && name2 != name)
+//         {
+//             finished = true;
+//             Invoke("levelFinished", 1);
+//         }
+//     }
 
-        // speechOut.Speak("Level 2... Turn the lower handle to select objects... Place three different objects by turning the upper handle.");
-        oS.soundLocked = true;
-        speechOut.Speak("Find the key to open the door. ");
-        Invoke("unlockSound", 10);
-        Invoke("saySecondPart", 20);
-    }
+//     private void levelFinished()
+//     {
+//         // speechOut.Speak("Well done! Move the handles in the middle to continue to the next level");
+//         speechOut.Speak("Sehr gut! Bewege beide Griffe in die Mitte um in das nächste Level zu kommen.");
+//         Invoke("finish", 5);
+//     }
 
-    public void objectPlaced(string name)
-    {
-        if (name1 == "")
-        {
-            name1 = name;
-        }
-        else if (name2 == "")
-        {
-            name2 = name;
-        }
-        else if (!finished && name1 != name && name2 != name)
-        {
-            finished = true;
-            Invoke("levelFinished", 1);
-        }
-    }
+//     private void finish()
+//     {
+//         GameObject.FindGameObjectsWithTag("PlayArea")[0].transform.position = new Vector3(0, 0, -10);
+//     }
 
-    private void levelFinished()
-    {
-        // speechOut.Speak("Well done! Move the handles in the middle to continue to the next level");
-        speechOut.Speak("Sehr gut! Bewege beide Griffe in die Mitte um in das nächste Level zu kommen.");
-        Invoke("finish", 5);
-    }
+//     private void unlockSound()
+//     {
+//         oS.soundLocked = false;
+//     }
 
-    private void finish()
-    {
-        GameObject.FindGameObjectsWithTag("PlayArea")[0].transform.position = new Vector3(0, 0, -10);
-    }
-
-    private void unlockSound()
-    {
-        oS.soundLocked = false;
-    }
-
-    private void saySecondPart()
-    {
-        speechOut.Speak("Bewege den oberen Griff an eine Position im Raum und platziere dort das ausgewählte Objekt durch leichtes drehen des oberen Griffs.");
-    }
-}
+//     private void saySecondPart()
+//     {
+//         speechOut.Speak("Bewege den oberen Griff an eine Position im Raum und platziere dort das ausgewählte Objekt durch leichtes drehen des oberen Griffs.");
+//     }
+// }
